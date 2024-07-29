931b2fc443b92841d7c1acce60065cd5d2dbf318
akupelikilinc <ahmetkupelikilinc@gmail.com>
2024-07-30 00:03:09 +0300

    Component Partial Yapısı Kuruldu.

---
+++ MyWebsite/ViewComponents/AboutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebsite.ViewComponents
+{
+    public class AboutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(); 
+        }
+    }
+}
